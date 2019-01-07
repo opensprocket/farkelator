c00aa9489bca434555eb97e0305d01385879329c
opensprocket <opensprocket@users.noreply.github.com>
2019-01-06 22:31:05 -0700

    branch creation
    inital commit of prototype (very rought one at that)

---
+++ revision 1/farkleator.js
@@ -0,0 +1,51 @@
+/* 
+===================================
+ Farkleator: The Farkle Calculator
+===================================
+Version: 1.0
+Last updated: 6 Dec 2019
+Designed by Josh Breiter
+===================================
+*/
+//Globbal Variables
+
+var playerList = []; //player list
+
+/*
+Player
+*/
+
+//create player
+function createPlayer(){
+    document.getElementById('playername');
+    
+}
+
+// add players to playerList
+function addPlayer(name){
+    playerList[playerList.length] = name;
+    console.log("Added " + name + " to the player list.");
+}
+
+
+
+/* Maybe this will be useful?
+<datalist id="player-list">
+    <option value="Josh">Josh</option>
+    <option value="Mom">Mom</option>
+</datalist>
+*/
+
+// start game
+
+// turn counter
+
+// score storage
+
+// scoreboard generator
+
+// score calculator
+
+// final turn/win condition
+
+// winning player
